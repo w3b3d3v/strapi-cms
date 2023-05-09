2533a814932ee80e8bf8c7bc4ee037e0a174314a
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-05-09 16:15:43 -0300

    [fix] creating new createOrUpdate to prevent duplicate record

--- src/api/video/controllers/video.js
+++ src/api/video/controllers/video.js
@@ -6,4 +6,17 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::video.video');
+module.exports = createCoreController('api::video.video', {
+    async createOrUpdate(ctx) {
+        const { video_id, ...data } = ctx.request.body;
+        const existingRecord = await strapi.services.video.findOne({ video_id });
+      
+        if (existingRecord) {
+          const updatedRecord = await strapi.services.video.update({ video_id }, data);
+          return updatedRecord;
+        } else {
+          const newRecord = await strapi.services.video.create(data);
+          return newRecord;
+        }
+      }      
+});
