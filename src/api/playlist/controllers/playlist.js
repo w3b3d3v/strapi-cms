31fff939fbcdb94d906d3278bf006cb0d885b019
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-05-10 09:55:17 -0300

    [fix] adjusting duplicates for playlist

--- src/api/playlist/controllers/playlist.js
+++ src/api/playlist/controllers/playlist.js
@@ -6,4 +6,17 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::playlist.playlist');
+module.exports = createCoreController('api::playlist.playlist', {
+    async createOrUpdate(ctx) {
+        const { playlist_id, ...data } = ctx.request.body;
+        const existingRecord = await strapi.services.playlist.findOne({ playlist_id });
+      
+        if (existingRecord) {
+          const updatedRecord = await strapi.services.playlist.update({ playlist_id }, data);
+          return updatedRecord;
+        } else {
+          const newRecord = await strapi.services.playlist.create(data);
+          return newRecord;
+        }
+      }      
+});
